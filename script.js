e34d1a96d0024f0455a88c4dad3a50240c7c749d
Aryan Ghugare <aryanghugare75@gmail.com>
2026-01-04 22:22:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+const button = document.querySelector("#openModal");
+const modal = document.querySelector(".modal");
+const closeBtn = document.querySelector(".close-modal");
+
+button.addEventListener("click", (e) => {
+	e.preventDefault();
+	modal.style.display = "flex";
+});
+
+// Close when clicking close button
+closeBtn.addEventListener("click", () => {
+	modal.style.display = "none";
+});
+
+// Close when clicking outside modal-content
+modal.addEventListener("click", (e) => {
+	if (e.target === modal) {
+		modal.style.display = "none";
+	}
+});
